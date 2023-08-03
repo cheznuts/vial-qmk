07cc712757b0f434e585ff5525d6d3a92193d6b0
Samuel <103608945+cheznuts@users.noreply.github.com>
2023-08-03 15:22:31 -0700

    Update keymap.c

--- keyboards/ktec/daisy/keymaps/via/keymap.c
+++ keyboards/ktec/daisy/keymaps/via/keymap.c
@@ -46,6 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,  KC_BSPC,                   LOWER,   RAISE,   KC_RALT
     ),
 
+
     /* Function Layer 1 (Lower)
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
      * │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │
